2c4b5cfd133c9cce5bb333a3d36e643a0d3f2006
Leonardo Vidarte <lvidarte@gmail.com>
2013-07-25 17:57:12 -0300

    Refactoring on estrellas

--- estrellas.py
+++ estrellas.py
@@ -9,46 +9,65 @@ def estrella(lados, largo, angulo):
         turtle.right((360.0 / lados) + angulo)
         turtle.forward(largo)
 
-if __name__ == '__main__':
 
-    turtle.setup(800, 600)
+class Application(tk.Frame):
 
-    screen = turtle.Screen()
-    screen.title('Estrellas')
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid(sticky=tk.W+tk.E+tk.N+tk.S)
+        self.create_widgets()
 
-    lados = tk.Scale(screen._root, from_=5, to=50,
-                     label='lados', orient=tk.HORIZONTAL)
-    largo = tk.Scale(screen._root, from_=5, to=200,
-                    label='largo', orient=tk.HORIZONTAL)
-    angulo = tk.Scale(screen._root, from_=0, to=180,
-                      label='angulo', orient=tk.HORIZONTAL)
+    def create_widgets(self):
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
 
-    lados.set(15)
-    largo.set(100)
-    angulo.set(140)
+        self.canvas = turtle.ScrolledCanvas(self, 960, 540, 1920, 1080)
+        self.canvas.grid(row=0, column=0, sticky=tk.W+tk.E+tk.N+tk.S)
 
-    def dibujar(event):
-        estrella(lados.get(), largo.get(), angulo.get())
+        turtle._Screen._root = self
+        turtle._Screen._canvas = self.canvas
 
-    boton = tk.Button(screen._root, text="Empezar")
-    boton.bind('<Button-1>', dibujar)
+        self.screen = turtle.Screen()
+        turtle.TurtleScreen.__init__(self.screen, self.screen._canvas)
 
-    def borrar(event):
+        self.sidebar = tk.Frame()
+        self.sidebar.grid(row=0, column=1, sticky=tk.N)
+
+        self.lados = tk.Scale(self.sidebar, from_=5, to=50,
+                              label='lados', orient=tk.HORIZONTAL)
+        self.lados.grid(row=0, column=0)
+        self.lados.set(15)
+
+        self.largo = tk.Scale(self.sidebar, from_=5, to=200,
+                              label='largo', orient=tk.HORIZONTAL)
+        self.largo.grid(row=1, column=0)
+        self.largo.set(100)
+
+        self.angulo = tk.Scale(self.sidebar, from_=0, to=180,
+                               label='angulo', orient=tk.HORIZONTAL)
+        self.angulo.grid(row=2, column=0)
+        self.angulo.set(140)
+
+        self.begin = tk.Button(self.sidebar, text="Empezar")
+        self.begin.grid(row=3, column=0)
+        self.begin.bind('<Button-1>', self.draw)
+
+        self.reset = tk.Button(self.sidebar, text="erase")
+        self.reset.grid(row=4, column=0)
+        self.reset.bind('<Button-1>', self.erase)
+
+    def draw(self, event):
+        estrella(self.lados.get(), self.largo.get(), self.angulo.get())
+
+    def erase(self, event):
         turtle.reset()
         turtle.speed(10) # Fast
 
-    reset = tk.Button(screen._root, text="Borrar")
-    reset.bind('<Button-1>', borrar)
 
-    lados.grid(row=0, column=0)
-    largo.grid(row=0, column=1)
-    angulo.grid(row=0, column=2)
-    boton.grid(row=0, column=3)
-    reset.grid(row=0, column=4)
-
-    borrar(None)
-    turtle.mainloop()
-
-
-
+if __name__ == '__main__':
+    app = Application()
+    app.mainloop()
 
